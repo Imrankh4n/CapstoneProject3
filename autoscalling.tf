443d7ee5492de752f41e0b5058a96b8b3b7a107f
Imrankh4n <127673321+Imrankh4n@users.noreply.github.com>
2024-02-06 20:22:54 +0530

    Update autoscalling.tf

--- autoscalling.tf
+++ autoscalling.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "my_template"{
    name = "capstone-template"
-   image_id = "ami-0c7217cdde317cfec"
+   image_id = "ami-0277155c3f0ab2930"
    instace_type = "t2.micro"
    network_interfaces{
 	associate_public_ip_address = "true"
